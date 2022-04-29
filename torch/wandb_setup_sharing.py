6e3a83bbab0595c3b64c486d917814f850cfb97b
Dongwoo-Im <cksgus159@naver.com>
2022-04-29 17:04:02 +0000

    Add wandb_setup_sharing.py

---
+++ torch/wandb_setup_sharing.py
@@ -0,0 +1,23 @@
+import wandb
+
+def wandb_login():
+    wandb.login(key='your_key')
+
+def wandb_init(args):
+    wandb.init(
+        project="semantic_segmentation_",
+        entity="mg_generation",
+        name='_'.join([args.work_dir_exp.split('/')[-1], args.encoder, args.decoder]),
+        reinit=True,
+        config = {
+            "encoder": args.encoder,
+            "encoder_weights": args.encoder_weights,
+            "decoder": args.decoder,
+            
+            "criterion": args.criterion,
+
+            "epochs": args.epoch,
+            "learning_rate": args.lr,
+            "batch_size": args.batch_size
+        },
+    )
