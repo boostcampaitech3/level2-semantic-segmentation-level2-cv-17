f9e4e95f6e0bac83cacb98246e532e6c90758144
Dongwoo-Im <cksgus159@naver.com>
2022-04-30 16:54:51 +0000

    [feat] smp_model.py model diversity
    - I add additional decoder models in smp_model.py
    - Linknet has lowest miou_score in epoch 1, so I don't add Linknet

--- torch/smp_model.py
+++ torch/smp_model.py
@@ -3,15 +3,83 @@ import segmentation_models_pytorch as smp
 
 def build_model(args):
     decoder = getattr(smp, args.decoder)
-    model = decoder(
-        encoder_name=args.encoder,
-        encoder_weights=args.encoder_weights,
-        in_channels=3,
-        classes=11,
-        activation=None,
-        encoder_depth=5,
-        decoder_channels=[256, 128, 64, 32, 16]
-    )
+    if args.decoder in ['Unet', 'UnetPlusPlus']:
+        model = decoder(
+            encoder_name=args.encoder,
+            encoder_weights=args.encoder_weights,
+            encoder_depth=5,
+            decoder_channels=[256, 128, 64, 32, 16], # list length should be same with encoder_depth
+            decoder_attention_type=None,
+            classes=args.classes,
+            activation=None,
+        )
+    elif args.decoder == 'MAnet':
+        model = decoder(
+            encoder_name=args.encoder,
+            encoder_weights=args.encoder_weights,
+            encoder_depth=5,
+            decoder_channels=[256, 128, 64, 32, 16], # list length should be same with encoder_depth
+            decoder_pab_channels=64,
+            classes=args.classes,
+            activation=None,
+        )
+    elif args.decoder == 'FPN':
+        model = decoder(
+            encoder_name=args.encoder,
+            encoder_weights=args.encoder_weights,
+            encoder_depth=5,
+            decoder_pyramid_channels=256,
+            decoder_segmentation_channels=128,
+            decoder_merge_policy='add',
+            decoder_dropout=0.2,
+            classes=args.classes,
+            activation=None,
+            upsampling=4,
+        )
+    elif args.decoder == 'PSPNet':
+        model = decoder(
+            encoder_name=args.encoder,
+            encoder_weights=args.encoder_weights,
+            encoder_depth=3,
+            psp_out_channels=512,
+            psp_dropout=0.2,
+            classes=args.classes,
+            activation=None,
+            upsampling=8,
+        )
+    elif args.decoder == 'PAN':
+        model = decoder(
+            encoder_name=args.encoder,
+            encoder_weights=args.encoder_weights,
+            encoder_output_stride=16,
+            decoder_channels=32,
+            classes=args.classes,
+            activation=None,
+            upsampling=4,
+        )
+    elif args.decoder == 'DeepLabV3':
+        model = decoder(
+            encoder_name=args.encoder,
+            encoder_weights=args.encoder_weights,
+            encoder_depth=5,
+            decoder_channels=256,
+            classes=args.classes,
+            activation=None,
+            upsampling=8,
+        )
+    elif args.decoder == 'DeepLabV3Plus':
+        model = decoder(
+            encoder_name=args.encoder,
+            encoder_weights=args.encoder_weights,
+            encoder_depth=5,
+            encoder_output_stride=16,
+            decoder_channels=256,
+            decoder_atrous_rates=(12, 24, 36),
+            classes=args.classes,
+            activation=None,
+            upsampling=4,
+        )
+    
     preprocessing_fn = smp.encoders.get_preprocessing_fn(args.encoder, args.encoder_weights)
     
     return model, preprocessing_fn
