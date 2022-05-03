fdffbd9704d7c289afa6498dd5e8f558aee56f81
HyoSeok <dlgytjr99999@kookmin.ac.kr>
2022-05-03 12:38:49 +0000

    [Feat] Keep Colors at each classes
    Before we didn't compare json1 results with json2 results, because each class color don't fixed.

--- dataviz.py
+++ dataviz.py
@@ -4,13 +4,18 @@ import argparse
 def main(arg):
     dataset = fo.Dataset.from_dir(
         dataset_type=fo.types.COCODetectionDataset,
-        label_types = ["detections", "segmentations"],
+        label_types = ["detections","segmentations"],
         data_path=arg.data_dir,
         labels_path=arg.anno_dir,
         include_id  =True
     )
     session = fo.launch_app(dataset, port=arg.port, address="0.0.0.0")
+
+    session.config.color_pool = ["#7B241C", "#9B59B6 ","#AED6F1","#73C6B6","#2ECC71","#F7DC6F","#E59866","#AEB6BF","#1B2631","#D2B4DE"]
+    session.refresh()   
     session.wait()
+
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
