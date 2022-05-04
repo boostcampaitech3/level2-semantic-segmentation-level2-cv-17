8b8031869e6c59a39be1695e9a8cc869afcc7cfd
Dongwoo-Im <cksgus159@naver.com>
2022-05-04 02:18:24 +0000

    [feat] CE class weight

--- torch/loss.py
+++ torch/loss.py
@@ -2,8 +2,11 @@ from utils import *
 
 
 def get_loss(args):
+    args.class_weights = [0.0146, 0.0948, 0.0502, 0.1144, 0.1153, 0.1065, 0.0567, 0.1077, 0.0481, 0.154, 0.1378]
+    class_weights = torch.tensor(args.class_weights).float().to(args.device)
+
     if args.criterion == 'CE':
-        criterion = torch.nn.CrossEntropyLoss()
+        criterion = torch.nn.CrossEntropyLoss(weight=class_weights)
     elif args.criterion == 'jaccard':
         criterion = smp.losses.JaccardLoss('multiclass')
     elif args.criterion == 'dice':
